f6d372907447ed6d12c762fa624c96ecb1f0aa13
John Chen <kryptonite303@users.noreply.github.com>
2020-07-15 15:38:29 -0400

    feat: Initial functionality (#4)
    * feat: Add initial functionality
    
    * docs: Updates to meta

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,12 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_pip3(host):
+    cmd = host.run("pip3 --version")
+    assert cmd.succeeded
